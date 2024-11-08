3cdae7ab5663e5641702336808936b5c47ef71d4
DND-N3RD-Al3x <darklord9142@gmail.com>
2024-11-08 12:05:20 -0700

    exported from Skill Struck

---
+++ syntax_and_comments.py
@@ -0,0 +1,7 @@
+morning = input("What do you do in the morning?")
+#weeeee
+afternoon = input("What do you do in the afternoon?")
+evening = input("What do you do in the evening?")
+print(morning)
+print(afternoon)
+print(evening)
