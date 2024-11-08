f28ff3748535ce534a30794931fcdd13e308c201
DND-N3RD-Al3x <darklord9142@gmail.com>
2024-11-08 12:05:19 -0700

    exported from Skill Struck

---
+++ input/output.py
@@ -0,0 +1,4 @@
+order1 = input("what do you wnat to order: ")
+order2 = input("what do you wnat to order: ")
+order3 = input("what do you wnat to order: ")
+print("you ordered " + order1 + ", "+ order2 + ", and ", order3 +"!")
